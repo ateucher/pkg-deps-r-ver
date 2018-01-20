0eb217d409f7d81582a5f14dc279d1c09c52cea9
Andy Teucher <andy.teucher@gmail.com>
2018-01-19 21:52:30 -0800

    First version of app

---
+++ app.R
@@ -0,0 +1,113 @@
+library(shiny)
+library(purrr)
+library(dplyr)
+library(tidyr)
+library(desc)
+library(DT)
+
+get_dep_r_versions <- function(desc = NULL, dep_types = c("Depends", "Imports", "Suggests", "Enhances")) {
+  dep_types <- match.arg(dep_types, several.ok = TRUE)
+  deps <- get_deps(desc = desc, dep_types = dep_types)
+  if (!nrow(deps)) {
+    return(data_frame(package = character(0), type = character(0), r_version = character(0),
+                      r_major = integer(0), r_minor = integer(0), r_patch = integer(0)))
+  }
+  
+  map_dfr(deps$package, pkg_rver) %>%
+    left_join(deps, by = "package") %>%
+    select(package, type, r_version, everything()) %>%
+    arrange(desc(r_major), desc(r_minor), desc(r_patch))
+}
+
+get_deps <- function(desc, dep_types = c("Depends", "Imports", "Suggests", "Enhances")) {
+  dep_types <- match.arg(dep_types, several.ok = TRUE)
+  desc::desc_get_deps(desc) %>% 
+    filter(type %in% dep_types, 
+           !package %in% c(getOption("defaultPackages"), "R")) %>% 
+    select(-version)
+}
+
+get_desc_file <- function(pkg) {
+  desc_file <- system.file("DESCRIPTION", package = pkg)
+  
+  if (!file.exists(desc_file)) {
+    # message("No DESCRIPTION file locally for ", pkg, 
+    #         ".\n Attempting to get from https://github.com/cran/", pkg)
+    desc_file <- tempfile()
+    url <- paste0("https://raw.githubusercontent.com/cran/", 
+                  pkg, "/master/DESCRIPTION")
+    res <- tryCatch(suppressWarnings(
+      download.file(url, destfile = desc_file, quiet = TRUE)), 
+      error = function(e) NULL) # warning("Unable to get DESCRIPTION file for ", pkg))
+    if (res > 0) return(NULL)
+  }
+  desc_file
+}
+
+parse_rver <- function(x) {
+  just_ver <- gsub("(\\s*\\(?>?=\\s*)|\\)", "", x)
+  if (!length(just_ver)) return(rep(NA_integer_, 3))
+  unclass(as.package_version(just_ver))[[1]]
+}
+
+pkg_rver <- function(pkg) {
+  desc_file <- get_desc_file(pkg)
+  if (is.null(desc_file)) return(NULL)
+  
+  deps <- desc::desc_get_deps(desc_file)
+  if (is.null(deps)) {
+    R_ver_txt <- character(0)
+  } else {
+    R_ver_txt <- filter(deps, package == "R") %>% pull(version)
+  }
+  if (!length(R_ver_txt)) {
+    R_ver_txt <- NA_character_
+    R_ver <- NA_integer_
+  }  else {
+    R_ver <- parse_rver(R_ver_txt)
+  }
+  list(package = pkg, r_version = R_ver_txt, 
+       r_major = R_ver[1], r_minor = R_ver[2], r_patch = R_ver[3])
+}
+
+ui <- fluidPage(
+  
+  titlePanel("Find the R version specified in the dependencies of a package"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("pkg","Choose a package on CRAN", 
+                placeholder = "Name of package"),
+      p("Or upload a"),
+      fileInput("descFile", "DESCRIPTION file", accept = "text/plain"), 
+      checkboxGroupInput("depTypes", "Dependency Types", 
+                         choices = c("Depends", "Imports", "Suggests", "Enhances", "LinkingTo"), 
+                         selected = c("Depends", "Imports", "Suggests"))
+    ),
+    
+    mainPanel(
+      dataTableOutput("pkgdeps")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$pkgdeps <- renderDataTable({
+    desc_file <- input$descFile
+    if (is.null(desc_file)) {
+      req(input$pkg)
+      desc <- get_desc_file(input$pkg)
+    } else {
+      if (desc_file$name != "DESCRIPTION") 
+        stop("File must be called DESCRIPTION")
+      desc <- desc_file$datapath
+      updateTextInput(session, inputId = "pkg", value = "")
+    }
+    get_dep_r_versions(desc = desc,
+                       dep_types = input$depTypes)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
